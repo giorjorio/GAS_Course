dacddb667f62fe50da1ad60d6d4fc2a02f0c166c
giorjorio <105600836+giorjorio@users.noreply.github.com>
2025-08-04 15:46:35 +0300

    Adding XP to the Player State

--- Source/Aura/Public/Player/AuraPlayerState.h
+++ Source/Aura/Public/Player/AuraPlayerState.h
@@ -11,6 +11,9 @@
 class UAbilitySystemComponent;
 class UAttributeSet;
 
+DECLARE_MULTICAST_DELEGATE_OneParam(FOnPlayerStatChanged, int32 /*StatValue*/);
+
+
 /**
  * 
  */
@@ -26,7 +29,21 @@ public:
 	
 	UAttributeSet* GetAttributeSet() const { return AttributeSet; }
 
+	FOnPlayerStatChanged OnXPChangedDelegate;
+	FOnPlayerStatChanged OnLevelChangedDelegate;
+	
+
 	FORCEINLINE int32 GetPlayerLevel() const { return Level; }
+	FORCEINLINE int32 GetPlayerXP() const { return XP; }
+
+	
+	
+	void SetLevel(int32 InLevel);
+	void SetXP(int32 InXP);
+	
+	void AddToLevel(int32 InLevel);
+	void AddToXP(int32 InXP);
+	
 
 
 
@@ -45,5 +62,12 @@ private:
 
 	UFUNCTION()
 	void OnRep_Level(int32 OldLevel);
+
+	UPROPERTY(EditAnywhere, ReplicatedUsing = OnRep_XP)
+	int32 XP = 1;
+
+	UFUNCTION()
+	void OnRep_XP(int32 OldXP);
+
 	
 };
